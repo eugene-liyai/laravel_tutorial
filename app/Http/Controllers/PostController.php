ad1999b5bbfe6b8ec604217d8200031c920e6d6e
e-liyai <eugene.liyai@andela.com>
2018-04-22 21:16:52 +0300

    [Feat: ] add post and update controllers

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,43 @@ class PostController extends Controller
         $posts = $post->getPosts($session);
         return view('blog.index', ['posts' => $posts]);
     }
+
+    public function getAdminIndex(Store $session) {
+        $post = new Post();
+        $posts = $post -> getPosts($session);
+        return view('admin.index', ['posts' => $posts]);
+    }
+
+    public function getPost(Store $session, $id) {
+        $post = new Post();
+        $post = $post -> getPosts($session, $id);
+        return view('blog.post', ['posts' => $post]);
+    }
+
+    public function getAdminCreate() {
+        return view('admin.create');
+    }
+
+    public function getAdminEdit(Store $session, $id) {
+        $post = new Post();
+        $post = $post->($session, $id);
+        return view('admin.edit', ['post' => $post, 'postId' => $id]);
+    }
+
+    public function postAdminCreate(Store $session, Request $request) {
+        $post = new Post();
+        $post -> addPost($session, $request -> input('title'), $request -> input('content'));
+        return redirect() -> route('admin.index')
+            -> with('info', 'Post created, Title is: '. $request -> input('title'));
+    }
+
+    public function postAdminUpdate(Store $session, Request $request) {
+        $post = new Post();
+        $post -> editPost($session,
+            $request -> input('id'),
+            $request -> input('title'),
+            $request -> input('content'));
+        return redirect() -> route('admin.index')
+            -> with('info', 'Post edited, new Title is: '. $request -> input('title'));
+    }
 }
